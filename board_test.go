ee1efbe4c35c718b4434ebfb1f4557897e6e1b9a
Sean McCullough <banksean@gmail.com>
2018-02-14 19:39:37 -0800

    ¯\_(ツ)_/¯

--- board_test.go
+++ board_test.go
@@ -16,85 +16,42 @@ func TestNewBoard(t *testing.T) {
 
 func TestScoreAt(t *testing.T) {
 	Convey("spot checks", t, func() {
-		So(ScoreAtConditional(0, 0), ShouldEqual, TW)
-		So(ScoreAtConditional(1, 1), ShouldEqual, DW)
-		So(ScoreAtConditional(2, 2), ShouldEqual, DW)
-		So(ScoreAtConditional(3, 3), ShouldEqual, DW)
-		So(ScoreAtConditional(4, 4), ShouldEqual, DW)
-		So(ScoreAtConditional(5, 5), ShouldEqual, TL)
-		So(ScoreAtConditional(6, 6), ShouldEqual, DL)
-		So(ScoreAtConditional(7, 7), ShouldEqual, DW)
-		So(ScoreAtConditional(0, 3), ShouldEqual, DL)
-		So(ScoreAtConditional(3, 0), ShouldEqual, DL)
+		So(ScrabbleScores.ScoreAt(0, 0), ShouldEqual, TW)
+		So(ScrabbleScores.ScoreAt(1, 1), ShouldEqual, DW)
+		So(ScrabbleScores.ScoreAt(2, 2), ShouldEqual, DW)
+		So(ScrabbleScores.ScoreAt(3, 3), ShouldEqual, DW)
+		So(ScrabbleScores.ScoreAt(4, 4), ShouldEqual, DW)
+		So(ScrabbleScores.ScoreAt(5, 5), ShouldEqual, TL)
+		So(ScrabbleScores.ScoreAt(6, 6), ShouldEqual, DL)
+		So(ScrabbleScores.ScoreAt(7, 7), ShouldEqual, DW)
+		So(ScrabbleScores.ScoreAt(0, 3), ShouldEqual, DL)
+		So(ScrabbleScores.ScoreAt(3, 0), ShouldEqual, DL)
 	})
 
 	Convey("symmetry", t, func() {
 		for x := 0; x < 15; x++ {
 			for y := 0; y < 15; y++ {
 				Convey(fmt.Sprintf("%d, %d", x, y), func() {
-					So(ScoreAtConditional(x, y), ShouldEqual, ScoreAtConditional(y, x))
+					So(ScrabbleScores.ScoreAt(x, y), ShouldEqual, ScrabbleScores.ScoreAt(y, x))
 				})
 			}
 		}
-	})
-
-	Convey("bits and ints", t, func() {
-		for x := 0; x < 15; x++ {
-			for y := 0; y < 15; y++ {
-				Convey(fmt.Sprintf("%d, %d", x, y), func() {
-					So(ScoreAtBits(x, y), ShouldEqual, ScoreAtConditional(x, y))
-					So(ScoreAtInt(x, y), ShouldEqual, ScoreAtConditional(x, y))
-					So(ScoreAtLiteral(x, y), ShouldEqual, ScoreAtConditional(x, y))
-				})
-			}
-		}
-		Printf("NoneInt: %x\n", NoneInt)
 	})
 }
 
 func TestScoreAcross(t *testing.T) {
 	Convey("basic", t, func() {
 		So(ScoreAcross(0, 0, "OH"), ShouldEqual, 15)
-		So(ScoreAtConditional(3, 7), ShouldEqual, DL)
+		So(ScrabbleScores.ScoreAt(3, 7), ShouldEqual, DL)
 		So(ScoreAcross(3, 7, "QUANT"), ShouldEqual, 48)
 	})
 }
 
-func BenchmarkScoreAtLiteral(b *testing.B) {
+func BenchmarkScrabbleScoresScoreAt(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		for x := 0; x < 15; x++ {
 			for y := 0; y < 15; y++ {
-				_ = ScoreAtLiteral(x, y)
-			}
-		}
-	}
-}
-
-func BenchmarkScoreAtConditional(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		for x := 0; x < 15; x++ {
-			for y := 0; y < 15; y++ {
-				_ = ScoreAtConditional(x, y)
-			}
-		}
-	}
-}
-
-func BenchmarkScoreAtBits(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		for x := 0; x < 15; x++ {
-			for y := 0; y < 15; y++ {
-				_ = ScoreAtBits(x, y)
-			}
-		}
-	}
-}
-
-func BenchmarkScoreAtInt(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		for x := 0; x < 15; x++ {
-			for y := 0; y < 15; y++ {
-				_ = ScoreAtInt(x, y)
+				_ = ScrabbleScores.ScoreAt(x, y)
 			}
 		}
 	}
