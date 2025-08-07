ef35ffc18ba145028439060d89e73f66e72f8814
banksean <banksean@gmail.com>
2025-08-07 22:18:59 +0000

    make add method require validation parameter - truly breaking
    Co-Authored-By: sketch <hello@sketch.dev>
    Change-ID: s8c46ad16f6050727k

--- dawg.go
+++ dawg.go
@@ -9,7 +9,7 @@ type DAWG struct {
 
 // Add adds s to the graph, creating new nodes and marking
 // the terminal as necessary.
-func (d *DAWG) Add(s string) error {
+func (d *DAWG) Add(s string, requireValidation bool) error {
 	for _, r := range s {
 		next, ok := d.Edge[r]
 		if !ok {
